c021a1cf2230d0cfed0f3ed214aa283dfe5365b3
Eric Leblond <eric@regit.org>
2011-09-24 20:38:09 +0200

    Add license.

--- suriwire.lua
+++ suriwire.lua
@@ -1,3 +1,28 @@
+-- suriwire
+-- A wireshark plugin to integrate suricata alerts in wireshark
+-- pcap output.
+--
+-- (c) 2011 Eric Leblond <eric@regit.org>
+--
+-- Wireshark - Network traffic analyzer
+-- By Gerald Combs <gerald@wireshark.org>
+-- Copyright 1998 Gerald Combs
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 3
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+
 suri_alerts = {}
 suri_proto = Proto("suricata","Suricata Postdissector")
 -- create a function to "postdissect" each frame
