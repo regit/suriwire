6d5ff2917a21293490868d491a37784559c2503a
Eric Leblond <eric@regit.org>
2011-09-29 14:08:53 +0200

    Use local function to avoid code run.
    If functions for suri_proto are not local to suriwire_activate(),
    it leads to an evaluation we don't want.

--- suriwire.lua
+++ suriwire.lua
@@ -24,47 +24,44 @@
 
 
 if (gui_enabled()) then 
-	suri_alerts = {}
-	suri_file = "sample.log"
-	local suri_proto = Proto("suricata","Suricata Analysis")
-	-- create a function to "postdissect" each frame
-	local suri_sid = ProtoField.string("suricata.sid", "SID", FT_STRING)
-	local suri_msg = ProtoField.string("suricata.msg", "Message", FT_STRING)
-
-	function suri_proto.dissector(buffer,pinfo,tree)
-	     for i, alert in ipairs(suri_alerts) do
-		  a = pinfo.number - alert[1]
-		  if (pinfo.number - alert[1] == 0) then
-		     subtree = tree:add(suri_proto, buffer[0])
-		     -- add protocol fields to subtree
-		     subtree:add(suri_msg, "SID: " .. alert[2] .. ": "):append_text(alert[3])
-		     subtree:add_expert_info(PI_MALFORMED, PI_WARN, alert[3])
-		     break
-		  end
-	     end
-	end
-
-	function suri_proto.init()
-	    local pat = "(%d+):(%d+):(.*)"
-	    -- read the lines in table 'lines'
-	    for line in io.lines() do
-	      local alert = {}
-	      for id, sid, text in string.gmatch(line, pat) do
-		  table.insert(alert, id)
-		  table.insert(alert, sid)
-		  table.insert(alert, text)
-	      end
-	      table.insert(suri_alerts, alert)
-	    end
-	end
-
 	-- register our protocol as a postdissector
 	function suriwire_activate()
+		suri_alerts = {}
+		local suri_proto = Proto("suricata","Suricata Analysis")
+		-- create a function to "postdissect" each frame
+		local suri_sid = ProtoField.string("suricata.sid", "SID", FT_STRING)
+		local suri_msg = ProtoField.string("suricata.msg", "Message", FT_STRING)
+
+		function suri_proto.dissector(buffer,pinfo,tree)
+		     for i, alert in ipairs(suri_alerts) do
+			  a = pinfo.number - alert[1]
+			  if (pinfo.number - alert[1] == 0) then
+			     subtree = tree:add(suri_proto, buffer[0])
+			     -- add protocol fields to subtree
+			     subtree:add(suri_msg, "SID: " .. alert[2] .. ": "):append_text(alert[3])
+			     subtree:add_expert_info(PI_MALFORMED, PI_WARN, alert[3])
+			     break
+			  end
+		     end
+		end
+
+		function suri_proto.init()
+		    local pat = "(%d+):(%d+):(.*)"
+		    -- read the lines in table 'lines'
+		    for line in io.lines() do
+		      local alert = {}
+		      for id, sid, text in string.gmatch(line, pat) do
+			  table.insert(alert, id)
+			  table.insert(alert, sid)
+			  table.insert(alert, text)
+		      end
+		      table.insert(suri_alerts, alert)
+		    end
+		end
 		function suriwire_register(file)
-			suri_file = file
-			print (file)
-	    		io.input(suri_file)
+	    		io.input(file)
 			register_postdissector(suri_proto)
+			reload()
 		end
 		-- run suricata
 		-- set input file
