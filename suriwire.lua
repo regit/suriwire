b077588321f106031d70becd718f0890b240ec2b
Eric Leblond <eric@regit.org>
2011-10-01 09:21:01 +0200

    Choose indentation style and apply it.
    I've choosen a no expand tab, tab width to 8 indentation.

--- suriwire.lua
+++ suriwire.lua
@@ -24,11 +24,12 @@
 
 
 if (gui_enabled()) then 
-	local suri_proto = Proto("suricata","Suricata Analysis")
+	local suri_proto = Proto("suricata", "Suricata Analysis")
 	local suri_sid = ProtoField.string("suricata.sid", "SID", FT_INTEGER)
 	local suri_msg = ProtoField.string("suricata.msg", "Message", FT_STRING)
 	local suri_prefs = suri_proto.prefs
-	suri_prefs.alert_file = Pref.string("Alert file", "alert-num.log", "Alert file containing information about pcap") 
+	suri_prefs.alert_file = Pref.string("Alert file", "alert-num.log",
+					    "Alert file containing information about pcap")
 	suri_proto.fields = {suri_sid, suri_msg}
 	-- register our protocol as a postdissector
 	function suriwire_activate()
@@ -36,7 +37,8 @@ if (gui_enabled()) then
 		function suri_proto.dissector(buffer,pinfo,tree)
 		     if not(suri_alerts[pinfo.number] == nil) then
 		             for i, val in ipairs(suri_alerts[pinfo.number]) do
-				     subtree = tree:add(suri_proto, "SID: "..val['sid'].." ("..val['msg']..")")
+				     subtree = tree:add(suri_proto,
+							"SID: "..val['sid'].." ("..val['msg']..")")
 				     -- add protocol fields to subtree
 				     subtree:add(suri_sid, val['sid'])
 				     subtree:add(suri_msg, val['msg'])
@@ -49,20 +51,21 @@ if (gui_enabled()) then
 		end
 
 		function suriwire_parser(file)
-                    local id = 0
-		    local s_text = ""
-		    local pat = "(%d+):(%d+):0:0:([^\n]*)"
-		    for s_text in io.lines(file) do
-			i, sid, text = string.match(s_text, pat)
-			id = tonumber(i)
-			if (i) then
-				if suri_alerts[id] == nil then
-					suri_alerts[id] = {}
-				end
+			local id = 0
+			local s_text = ""
+			local pat = "(%d+):(%d+):0:0:([^\n]*)"
+			for s_text in io.lines(file) do
+				i, sid, text = string.match(s_text, pat)
+				id = tonumber(i)
+				if (i) then
+					if suri_alerts[id] == nil then
+						suri_alerts[id] = {}
+					end
 				table.insert(suri_alerts[id], {sid = sid, msg = text})
+				end
 			end
-		    end
 		end
+
 		function suriwire_register(file)
 			if file == "" then
 				file = suri_prefs.alert_file
@@ -78,12 +81,17 @@ if (gui_enabled()) then
 				-- seems autoloading is done
 				reload()
 			else
-				new_dialog("Unable to open '" .. file .. "'. Choose another alert file", suriwire_register, "Choose file (default:" .. suri_prefs.alert_file..")")
+				new_dialog("Unable to open '" .. file
+					   .. "'. Choose another alert file",
+					   suriwire_register,
+					   "Choose file (default:" .. suri_prefs.alert_file..")")
 			end
 		end
 		-- run suricata
 		-- set input file
-		new_dialog("Choose alert file", suriwire_register, "Choose file (default:" .. suri_prefs.alert_file..")")
+		new_dialog("Choose alert file",
+			   suriwire_register,
+			   "Choose file (default:" .. suri_prefs.alert_file..")")
 		-- debug 1.7 
 		-- suriwire_register("sample.log")
 	end
@@ -97,4 +105,3 @@ if (gui_enabled()) then
 	-- debug 1.7
 	-- suriwire_activate()
 end
-
