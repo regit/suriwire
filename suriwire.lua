9ef8e0b4b9dc6e739ba98bf1ac9feb6128204bd5
Eric Leblond <eric@regit.org>
2011-10-03 00:37:03 +0200

    Follow numlog output evolution.
    This patch follow numlog output module evolution by adding a bunch
    of fields to the protocol.

--- suriwire.lua
+++ suriwire.lua
@@ -25,13 +25,23 @@
 
 if (gui_enabled()) then 
 	local suri_proto = Proto("suricata", "Suricata Analysis")
+	local suri_gid = ProtoField.string("suricata.gid", "GID", FT_INTEGER)
 	local suri_sid = ProtoField.string("suricata.sid", "SID", FT_INTEGER)
+	local suri_rev = ProtoField.string("suricata.rev", "Rev", FT_INTEGER)
 	local suri_msg = ProtoField.string("suricata.msg", "Message", FT_STRING)
+	local suri_flow = ProtoField.string("suricata.flow", "Flow flag", FT_BOOLEAN)
+	local suri_tx_id = ProtoField.string("suricata.tx_id", "Transmit ID", FT_INTEGER)
+	local suri_to_server = ProtoField.string("suricata.to_server", "Flow is to server", FT_BOOLEAN)
+	local suri_to_client = ProtoField.string("suricata.to_cient", "Flow is to client", FT_BOOLEAN)
 	local suri_prefs = suri_proto.prefs
 	local suri_running = false
-	suri_prefs.alert_file = Pref.string("Alert file", "alert-num.log",
+	suri_prefs.suri_command = Pref.string("Suricata binary", "/usr/bin/suricata",
+					    "Path to suricata binary")
+	suri_prefs.config_file = Pref.string("Suricata configuration", "/etc/suricata/suricata.yaml",
 					    "Alert file containing information about pcap")
-	suri_proto.fields = {suri_sid, suri_msg}
+	suri_prefs.alert_file = Pref.string("Alert file", "/var/log/suricata/alert-num.log",
+					    "Numlog alert file containing information about pcap")
+	suri_proto.fields = {suri_gid, suri_sid, suri_rev, suri_msg, suri_flow, suri_tx_id, suri_to_server, suri_to_client}
 	-- register our protocol as a postdissector
 	function suriwire_activate()
 		local suri_alerts = {}
@@ -41,8 +51,14 @@ if (gui_enabled()) then
 				     subtree = tree:add(suri_proto,
 							"SID: "..val['sid'].." ("..val['msg']..")")
 				     -- add protocol fields to subtree
+				     subtree:add(suri_gid, val['gid'])
 				     subtree:add(suri_sid, val['sid'])
+				     subtree:add(suri_rev, val['rev'])
 				     subtree:add(suri_msg, val['msg'])
+				     subtree:add(suri_flow, val['flow'])
+				     subtree:add(suri_tx_id, val['tx_id'])
+				     subtree:add(suri_to_client, val['to_client'])
+				     subtree:add(suri_to_server, val['to_server'])
 				     subtree:add_expert_info(PI_MALFORMED, PI_WARN, val['msg'])
 			     end
 		     end
@@ -54,16 +70,36 @@ if (gui_enabled()) then
 		function suriwire_parser(file)
 			local id = 0
 			local s_text = ""
-			local pat = "(%d+):(%d+):0:0:([^\n]*)"
+			local pat = "(%d+):(%d+):(%d+):(%d+):(%d):(%d):(%d):(%d+):0:0:([^\n]*)"
 			suri_alerts = {}
 			for s_text in io.lines(file) do
-				i, sid, text = string.match(s_text, pat)
+				i, gid, sid, rev, flow, to_server, to_client, tx_id, text = string.match(s_text, pat)
 				id = tonumber(i)
 				if (i) then
 					if suri_alerts[id] == nil then
 						suri_alerts[id] = {}
 					end
-				table.insert(suri_alerts[id], {sid = sid, msg = text})
+				table.insert(suri_alerts[id],
+					     {gid = tonumber(gid), sid = tonumber(sid),
+					      rev = tonumber(rev), flow = flow, to_server = to_server,
+					      to_client = to_client, tx_id = tonumber(tx_id), msg = text})
+				end
+			end
+		end
+
+		function suriwire_run(file)
+			local suri_command = suri_prefs.suri_command .. " -c " ..
+					     suri_prefs.config_file .. " -r " ..
+					     file
+			-- TODO Progress dialog
+			suri_return, suri_status = os.execute(suri_command)
+			-- if command is run succesfully we will have a log file
+			if suri_status == 1 then
+				-- TODO Text window with output
+				print("Unable to run command:" .. suri_return)
+			else
+				if suri_prefs.copy_alert_file then
+					io.output(file .. ".log"):write(io.input(suri_prefs.alert_file):read("*all"))
 				end
 			end
 		end
