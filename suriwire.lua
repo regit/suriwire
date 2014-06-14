f89a85681c09518a730cfc1eae0e886da7cb22dd
Eric Leblond <eric@regit.org>
2014-06-14 11:01:07 +0200

    Use EVE output instead of pcapinfo
    Suricata 2.0 features EVE JSON output which contains all the necessary info
    to add alert related disseector and more. This patch parses eve.json file
    and display information about alerts in wireshark.

--- suriwire.lua
+++ suriwire.lua
@@ -24,6 +24,7 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
+local json = require("cjson")
 
 if (gui_enabled()) then 
 	local suri_proto = Proto("suricata", "Suricata Analysis")
@@ -31,17 +32,17 @@ if (gui_enabled()) then
 	local suri_sid = ProtoField.string("suricata.sid", "SID", FT_INTEGER)
 	local suri_rev = ProtoField.string("suricata.rev", "Rev", FT_INTEGER)
 	local suri_msg = ProtoField.string("suricata.msg", "Message", FT_STRING)
-	local suri_flow = ProtoField.string("suricata.flow", "Flow flag", FT_BOOLEAN)
-	local suri_to_server = ProtoField.string("suricata.to_server", "Flow is to server", FT_BOOLEAN)
-	local suri_to_client = ProtoField.string("suricata.to_client", "Flow is to client", FT_BOOLEAN)
+	--local suri_flow = ProtoField.string("suricata.flow", "Flow flag", FT_BOOLEAN)
+	--local suri_to_server = ProtoField.string("suricata.to_server", "Flow is to server", FT_BOOLEAN)
+	--local suri_to_client = ProtoField.string("suricata.to_client", "Flow is to client", FT_BOOLEAN)
 	local suri_prefs = suri_proto.prefs
 	local suri_running = false
 	-- suri_prefs.suri_command = Pref.string("Suricata binary", "/usr/bin/suricata",
 	--				    "Path to suricata binary")
 	-- suri_prefs.config_file = Pref.string("Suricata configuration", "/etc/suricata/suricata.yaml",
 	--				    "Alert file containing information about pcap")
-	suri_prefs.alert_file = Pref.string("Alert file", "/var/log/suricata/alert-pcapinfo.log",
-					    "Numlog alert file containing information about pcap")
+	suri_prefs.alert_file = Pref.string("EVE file", "/var/log/suricata/eve.json",
+					    "EVE file containing information about pcap")
 	-- suri_prefs.copy_alert_file = Pref.bool("Make a copy of alert file", true,
 	--				       "When running suricata, create a copy of alert"
 	--				       .. " file in the directory of the pcap file")
@@ -59,9 +60,9 @@ if (gui_enabled()) then
 				     subtree:add(suri_sid, val['sid'])
 				     subtree:add(suri_rev, val['rev'])
 				     subtree:add(suri_msg, val['msg'])
-				     subtree:add(suri_flow, val['flow'])
-				     subtree:add(suri_to_client, val['to_client'])
-				     subtree:add(suri_to_server, val['to_server'])
+				     --subtree:add(suri_flow, val['flow'])
+				     --subtree:add(suri_to_client, val['to_client'])
+				     --subtree:add(suri_to_server, val['to_server'])
 				     subtree:add_expert_info(PI_MALFORMED, PI_WARN, val['msg'])
 			     end
 		     end
@@ -70,22 +71,44 @@ if (gui_enabled()) then
 		function suri_proto.init()
 		end
 
+		-- function suriwire_parser(file)
+		-- 	local id = 0
+		-- 	local s_text = ""
+		-- 	local pat = "(%d+):(%d+):(%d+):(%d+):(%d):(%d):(%d):0:0:([^\n]*)"
+		-- 	suri_alerts = {}
+		-- 	for s_text in io.lines(file) do
+		-- 		i, gid, sid, rev, flow, to_server, to_client, text = string.match(s_text, pat)
+		-- 		id = tonumber(i)
+		-- 		if (i) then
+		-- 			if suri_alerts[id] == nil then
+		-- 				suri_alerts[id] = {}
+		-- 			end
+		-- 		table.insert(suri_alerts[id],
+		-- 			     {gid = tonumber(gid), sid = tonumber(sid),
+		-- 			      rev = tonumber(rev), flow = flow, to_server = to_server,
+		-- 			      to_client = to_client, tx_id = tonumber(tx_id), msg = text})
+		-- 		end
+		-- 	end
+		-- end
+
 		function suriwire_parser(file)
+			local event
 			local id = 0
 			local s_text = ""
-			local pat = "(%d+):(%d+):(%d+):(%d+):(%d):(%d):(%d):0:0:([^\n]*)"
 			suri_alerts = {}
 			for s_text in io.lines(file) do
-				i, gid, sid, rev, flow, to_server, to_client, text = string.match(s_text, pat)
-				id = tonumber(i)
-				if (i) then
-					if suri_alerts[id] == nil then
-						suri_alerts[id] = {}
+				event = json.decode(s_text)
+				if event["event_type"] == "alert" then
+					id = event["pcap_cnt"]
+					if not (id == nil) then
+						print (id)
+						if suri_alerts[id] == nil then
+							suri_alerts[id] = {}
+						end
+						table.insert(suri_alerts[id],
+							{gid = tonumber(event["alert"]["gid"]), sid = tonumber(event["alert"]["signature_id"]),
+							rev = tonumber(event["alert"]["rev"]), msg = event["alert"]["signature"]})
 					end
-				table.insert(suri_alerts[id],
-					     {gid = tonumber(gid), sid = tonumber(sid),
-					      rev = tonumber(rev), flow = flow, to_server = to_server,
-					      to_client = to_client, tx_id = tonumber(tx_id), msg = text})
 				end
 			end
 		end
